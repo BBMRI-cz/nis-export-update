13e723efb9f252d0c6cac96091b02da399e45382
Matúš Formánek <525111@mail.muni.cz>
2026-04-22 17:49:23 +0200

    chore: add migrations

--- src/infrastructure/db/session.py
+++ src/infrastructure/db/session.py
@@ -1,8 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://user:password@localhost:5432/nis"
+import os
 
-engine = create_engine(DATABASE_URL, echo=False)
+
+def require_env(name: str) -> str:
+    value = os.getenv(name)
+    if not value:
+        raise RuntimeError(f"Missing required environment variable: {name}")
+    return value
+
+# needs to be a function for migrations to work
+def get_database_url() -> str:
+    return (
+        f"postgresql+psycopg2://"
+        f"{require_env('POSTGRES_USER')}:{require_env('POSTGRES_PASSWORD')}"
+        f"@localhost:{require_env('POSTGRES_PORT')}"
+        f"/{require_env('POSTGRES_DB')}"
+    )
+
+
+engine = create_engine(get_database_url(), echo=False)
 
 SessionLocal = sessionmaker(bind=engine)
